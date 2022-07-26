5955f9cca9efe58ab253323f3392c8d9ba3ac0f3
SunnyKrSingh <prioritys90@gmail.com>
2022-07-26 23:07:10 +0530

    division operation added

--- calculator.c
+++ calculator.c
@@ -32,6 +32,10 @@ int main()
         res = num1 * num2;
         printf("%d*%d = %d", num1, num2, res);
         break;
+    case 4:
+        res = num1 / num2;
+        printf("%d/%d = %d", num1, num2, res);
+        break;
     }
 
     return 0;
