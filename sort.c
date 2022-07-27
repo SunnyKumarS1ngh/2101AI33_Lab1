a53a0f516dd428b76c9f49316ad421a935fd5e29
SunnyKrSingh <prioritys90@gmail.com>
2022-07-27 11:22:48 +0530

    added selection sort

--- sort.c
+++ sort.c
@@ -15,10 +15,32 @@ void insertionSort(int arr[],int n){
         arr[j + 1] = key;
     }
 }
+//SelctionSort
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+
+    for (i = 0; i < n-1; i++)
+    {
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+          if (arr[j] < arr[min_idx])
+            min_idx = j;
+
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
 
 int main(){
   int arr[5] = {5,2,4,9,12};
-  insertionSort(arr,5);
+  selectionSort(arr,5);
   for(int i=0;i<5;i++){
     printf("%d,",arr[i]);
   }
