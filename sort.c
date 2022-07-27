677e5fcd081548c7cf5f2a9aba236c51d3364af2
SunnyKrSingh <prioritys90@gmail.com>
2022-07-27 11:27:51 +0530

    added merge sort

--- sort.c
+++ sort.c
@@ -52,10 +52,54 @@ void bubblesort(int a[],int n){
         }
     }
 }
+//merge sort
+void merge(int *a,int s,int e){
+    int mid = (s+e)/2;
 
+    int i = s;
+    int j = mid+1;
+    int k = s;
+
+    int temp[100];
+
+    while(i<=mid && j<=e){
+        if(a[i] < a[j]){
+            temp[k++] = a[i++];
+        }
+        else{
+            temp[k++] = a[j++];
+        }
+    }
+    while(i<=mid){
+        temp[k++] = a[i++];
+    }
+    while(j<=e){
+        temp[k++]  = a[j++];
+    }
+
+    for(int i=s;i<=e;i++){
+        a[i] = temp[i];
+    }
+
+
+}
+
+void mergeSort(int a[],int s,int e){
+    if(s>=e){
+        return;
+    }
+
+    int mid = (s+e)/2;
+
+    mergeSort(a,s,mid);
+    mergeSort(a,mid+1,e);
+
+    merge(a,s,e);
+
+}
 int main(){
-  int arr[5] = {5,2,4,9,12};
-  bubblesort(arr,5);
+  int arr[5] = {5,2,4,12,9};
+  mergeSort(arr,0,4);
   for(int i=0;i<5;i++){
     printf("%d,",arr[i]);
   }
