37ca8da8c470d434b954f57723bba0a4f4742a20
SunnyKrSingh <prioritys90@gmail.com>
2022-07-27 11:29:34 +0530

    added Quick sort

--- sort.c
+++ sort.c
@@ -97,9 +97,33 @@ void mergeSort(int a[],int s,int e){
     merge(a,s,e);
 
 }
+
+//Quick sort
+int partition(int a[],int s,int e){
+     int i=s;
+     int pivot = a[e];
+     for(int j=s;j<=e-1;j++){
+        if(a[j] < pivot){
+            swap(&a[i],&a[j]);
+            i++;
+        }
+     }
+     swap(&a[i],&a[e]);
+     return i;
+}
+
+void quickSort(int a[],int s,int e){
+    if(s>=e){
+        return;
+    }
+    int p = partition(a,s,e);
+    quickSort(a,s,p-1);
+    quickSort(a,p+1,e);
+}
+
 int main(){
   int arr[5] = {5,2,4,12,9};
-  mergeSort(arr,0,4);
+  quickSort(arr,0,4);
   for(int i=0;i<5;i++){
     printf("%d,",arr[i]);
   }
